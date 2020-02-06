b29e314c64dd85036b4b8a5abd861a5a33489b79
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-06 00:25:49 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -3,7 +3,7 @@ require 'pry'
 def start_game(player1, player2)
   "Hello #{player1} & #{playar2}"
 end
-
+start_game("Rocky", "Bullwinkle")
 def play_game(player2)
   "#{player1} is better than #{player2}"
 end
